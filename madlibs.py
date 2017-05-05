7180ad2627a806e8a60764b986a09d65f2f75145
Erlenbush <Stephen.erlenbush@gmail.com>
2017-05-05 13:35:31 -0700

    New python project
    First madlib

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+# Have the user enter a verb
+
+verb = input("Enter a verb: ")
+
+# Have the user enter a noun
+
+noun = input("Enter a noun: ")
+
+# Have the user enter a describing word
+
+describingWord = input("Enter a word you would use to describe something: ")
+
+# Have the user enter an adverb
+
+adverb = input("Now give me an adverb: ")
+
+# Here print out the madlib and profit!
+
+print("One bright, sunny day, you decided to go for a walk. On your walk you found a " + noun + ".")
+print("It was a really really " + describingWord + " " + noun + ".")
+print("You decided to take the " + noun + " and you " + adverb + " " + verb + " " + "with it until you get too tired.")
